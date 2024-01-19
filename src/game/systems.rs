23a826b5d20f7b0274886c8bbdea0d99e78da602
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2024-01-19 09:01:26 -0600

    update handling game state

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -3,18 +3,18 @@ use bevy::prelude::*;
 use crate::game::SimulationState;
 
 pub fn toggle_simulation(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
+    mut next_simulation_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         if simulation_state.0 == SimulationState::Running {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
+            next_simulation_state.set(SimulationState::Paused);
             println!("Pausing simulation...");
         } 
 
         if simulation_state.0 == SimulationState::Paused {
-            commands.insert_resource(NextState(Some(SimulationState::Running)));
+            next_simulation_state.set(SimulationState::Running);
             println!("Resuming simulation...");
         } 
     }
