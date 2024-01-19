2539cfdba7fbe502d89a8cf256424d31f6751f58
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2024-01-19 11:01:17 -0600

    some cleanup

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,13 +1,17 @@
 use bevy::prelude::*;
 
+mod components;
+mod styles;
+mod systems;
+
+use systems::layout::*;
+use crate::AppState;
+
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app.add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }
-}
-
-pub fn main_menu() {
-    println!("You are in the main menu!");
 }
